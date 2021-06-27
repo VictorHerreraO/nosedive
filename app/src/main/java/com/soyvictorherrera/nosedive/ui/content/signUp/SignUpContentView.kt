4da21cb448f5d352f5faa280b4cf22e7406875aa
VictorHerreraO <vherrera@overcome.mx>
2021-06-27 12:07:24 -0500

    Login: agregados links a TyC

--- app/src/main/java/com/soyvictorherrera/nosedive/ui/content/signUp/SignUpContentView.kt
+++ app/src/main/java/com/soyvictorherrera/nosedive/ui/content/signUp/SignUpContentView.kt
@@ -1,7 +1,11 @@
 package com.soyvictorherrera.nosedive.ui.content.signUp
 
+import android.text.Annotation
+import android.text.SpannedString
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
@@ -9,19 +13,21 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.sharp.ArrowBack
 import androidx.compose.material.icons.sharp.VisibilityOff
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,7 +35,9 @@ import androidx.compose.ui.unit.sp
 import com.soyvictorherrera.nosedive.R
 import com.soyvictorherrera.nosedive.ui.composable.common.DefaultProminentTopAppBar
 import com.soyvictorherrera.nosedive.ui.theme.Alto
+import com.soyvictorherrera.nosedive.ui.theme.Dove_Gray
 import com.soyvictorherrera.nosedive.ui.theme.NosediveTheme
+import com.soyvictorherrera.nosedive.ui.theme.Wild_Watermelon
 
 @Composable
 fun SignUpContent() {
@@ -123,10 +131,60 @@ fun SignUpContent() {
                         )
                     }
 
+                    val termsAndConditions =
+                        LocalContext.current.resources.getText(R.string.signup_terms_and_conditions) as SpannedString
+                    val annotations = termsAndConditions.getSpans(
+                        0,
+                        termsAndConditions.length,
+                        Annotation::class.java
+                    )
+                    val annotatedString by remember {
+                        mutableStateOf(with(AnnotatedString.Builder()) {
+                            Log.d("AnnotatedString", "building")
+                            append(termsAndConditions.toString())
+                            for (annotation in annotations) {
+                                if (annotation.key == "target") {
+                                    val annotationStart =
+                                        termsAndConditions.getSpanStart(annotation)
+                                    val annotationEnd = termsAndConditions.getSpanEnd(annotation)
+                                    addStyle(
+                                        style = SpanStyle(
+                                            color = Wild_Watermelon,
+                                            textDecoration = TextDecoration.Underline
+                                        ),
+                                        start = annotationStart,
+                                        end = annotationEnd
+                                    )
+                                    val target = annotation.value
+                                    Log.d("AnnotatedString", "target = $target")
+                                    addStringAnnotation(
+                                        tag = "URL",
+                                        annotation = "https://soyvictorherrera.com/?target=$target",
+                                        start = annotationStart,
+                                        end = annotationEnd
+                                    )
+                                }
+                            }
+                            toAnnotatedString()
+                        })
+                    }
+                    val uriHandler = LocalUriHandler.current
+
                     Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = "Al crear tu cuenta aceptas el aviso de privacidad y los términos y condiciones del servicio",
-                        style = MaterialTheme.typography.body2.copy(fontSize = 12.sp)
+                    ClickableText(
+                        text = annotatedString,
+                        modifier = Modifier.fillMaxWidth(),
+                        style = MaterialTheme.typography.body2.copy(
+                            color = Dove_Gray,
+                            fontSize = 12.sp
+                        ),
+                        onClick = { offset ->
+                            annotatedString.getStringAnnotations("URL", offset, offset)
+                                .firstOrNull()?.let { annotation ->
+                                    Log.d("OnClick", annotation.item)
+                                    uriHandler.openUri(annotation.item)
+                                }
+                        }
                     )
 
                     // Have an account button
