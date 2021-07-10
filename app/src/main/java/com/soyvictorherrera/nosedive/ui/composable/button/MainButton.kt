7e847be209582edb496b0f1758f663ec9a5aa34a
VictorHerreraO <vherrera@overcome.mx>
2021-07-10 14:35:25 -0500

    Composable: mostrar loader en MainButton

--- app/src/main/java/com/soyvictorherrera/nosedive/ui/composable/button/MainButton.kt
+++ app/src/main/java/com/soyvictorherrera/nosedive/ui/composable/button/MainButton.kt
@@ -1,13 +1,9 @@
 package com.soyvictorherrera.nosedive.ui.composable.button
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Done
 import androidx.compose.runtime.Composable
@@ -25,30 +21,46 @@ fun MainButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    icon: ImageVector? = null
+    icon: ImageVector? = null,
+    showLoader: Boolean = false
 ) {
     Button(
         onClick = { onClick() },
         modifier = modifier.fillMaxWidth(),
-        enabled = enabled
+        enabled = enabled && !showLoader
     ) {
-        Row {
-            val contentColor = Color.White
-            icon?.let { ic ->
-                Icon(
-                    imageVector = ic,
-                    contentDescription = null,
-                    modifier = Modifier.size(ButtonDefaults.IconSize),
-                    tint = contentColor
-                )
-                Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
-            }
-            Text(
-                text = text.toUpperCase(Locale.current),
-                color = contentColor
-            )
+        if (showLoader) {
+            LoadingButtonContent()
+        } else {
+            MainButtonContent(text = text, icon = icon)
         }
     }
+}
+
+@Composable
+fun MainButtonContent(
+    text: String,
+    icon: ImageVector? = null
+) {
+    val contentColor = Color.White
+    icon?.let { ic ->
+        Icon(
+            imageVector = ic,
+            contentDescription = null,
+            modifier = Modifier.size(ButtonDefaults.IconSize),
+            tint = contentColor
+        )
+        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+    }
+    Text(
+        text = text.toUpperCase(Locale.current),
+        color = contentColor
+    )
+}
+
+@Composable
+fun LoadingButtonContent() {
+    CircularProgressIndicator(modifier = Modifier.size(ButtonDefaults.IconSize))
 }
 
 @Preview("Main button preview")
