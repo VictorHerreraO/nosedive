9a9297a23c8768623b52afe77e8235371afe4e60
VictorHerreraO <vherrera@overcome.mx>
2021-10-20 09:50:54 -0500

    App: agregado timber

--- app/src/main/java/com/soyvictorherrera/nosedive/BaseApplication.kt
+++ app/src/main/java/com/soyvictorherrera/nosedive/BaseApplication.kt
@@ -2,6 +2,18 @@ package com.soyvictorherrera.nosedive
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class BaseApplication: Application()
+class BaseApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        installTimber()
+    }
+
+    private fun installTimber() {
+        if(!BuildConfig.DEBUG) return
+        Timber.plant(Timber.DebugTree())
+    }
+}
