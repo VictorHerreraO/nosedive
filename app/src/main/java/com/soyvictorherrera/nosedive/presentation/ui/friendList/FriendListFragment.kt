151dced6f3423aaf9539d3d8ae1e6a6813f4475d
VictorHerreraO <vherrera@overcome.mx>
2021-12-30 12:59:34 -0600

    ListaAmigos: responder a eventos
    Navegar a home
    Ir a perfil de amigo

--- app/src/main/java/com/soyvictorherrera/nosedive/presentation/ui/friendList/FriendListFragment.kt
+++ app/src/main/java/com/soyvictorherrera/nosedive/presentation/ui/friendList/FriendListFragment.kt
@@ -7,11 +7,15 @@ import android.view.ViewGroup
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.ComposeView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.soyvictorherrera.nosedive.R
 import com.soyvictorherrera.nosedive.presentation.theme.NosediveTheme
+import com.soyvictorherrera.nosedive.presentation.ui.Screen
+import com.soyvictorherrera.nosedive.presentation.ui.navigateInTo
+import com.soyvictorherrera.nosedive.presentation.ui.popUpTo
 import com.soyvictorherrera.nosedive.presentation.ui.shared.UserDetailsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
@@ -27,6 +31,20 @@ class FriendListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        viewModel.navigateTo.observe(viewLifecycleOwner) { navigateToEvent ->
+            navigateToEvent.getContentIfNotHandled()?.let { screen ->
+                val here = Screen.FriendList
+                when (screen) {
+                    Screen.Home -> popUpTo(screen)
+                    is Screen.FriendProfile -> navigateInTo(
+                        to = screen,
+                        from = here,
+                        args = bundleOf("user-id" to screen.userId)
+                    )
+                    else -> navigateInTo(to = screen, from = here)
+                }
+            }
+        }
 
         userDetailsViewModel.friendList.observe(viewLifecycleOwner) { friendList ->
             viewModel.onFriendListChanged(friendList)
@@ -56,10 +74,10 @@ class FriendListFragment : Fragment() {
     private fun onFriendListEvent(event: FriendListEvent) {
         when (event) {
             is FriendListEvent.FriendSelected -> {
-                Timber.i("friend [${event.friend.name}] selected")
+                viewModel.onFriendSelected(event.friend)
             }
             FriendListEvent.NavigateBack -> {
-                Timber.i("Navigate back")
+                viewModel.onNavigateBack()
             }
             FriendListEvent.SearchFriend -> {
                 Timber.i("Search friend here")
--- app/src/main/java/com/soyvictorherrera/nosedive/presentation/ui/friendList/FriendListViewModel.kt
+++ app/src/main/java/com/soyvictorherrera/nosedive/presentation/ui/friendList/FriendListViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.soyvictorherrera.nosedive.domain.model.FriendModel
+import com.soyvictorherrera.nosedive.presentation.ui.Event
+import com.soyvictorherrera.nosedive.presentation.ui.Screen
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -12,12 +14,26 @@ class FriendListViewModel @Inject constructor(
 
 ) : ViewModel() {
 
+    private val _navigateTo = MutableLiveData<Event<Screen>>()
+    val navigateTo: LiveData<Event<Screen>>
+        get() = _navigateTo
+
     private val _friendList = MutableLiveData<List<FriendModel>>()
     val friendList: LiveData<List<FriendModel>>
         get() = _friendList
 
     fun onFriendListChanged(friendList: List<FriendModel>) {
         _friendList.value = friendList.sortedBy { it.name }
+    }
+
+    fun onNavigateBack() {
+        _navigateTo.value = Event(Screen.Home)
+    }
+
+    fun onFriendSelected(friend: FriendModel) {
+        _navigateTo.value = Event(Screen.FriendProfile(
+            userId = friend.id
+        ))
     }
 
 }
