2b315d1e8c1bee6c7553172af58794537b33fe7e
VictorHerreraO <vherrera@overcome.mx>
2022-01-04 11:55:06 -0600

    CalificarAmigo: generar eventos de calificación

--- app/src/main/java/com/soyvictorherrera/nosedive/presentation/component/rate/RatingBar.kt
+++ app/src/main/java/com/soyvictorherrera/nosedive/presentation/component/rate/RatingBar.kt
@@ -1,59 +1,43 @@
 package com.soyvictorherrera.nosedive.presentation.component.rate
 
-import androidx.compose.foundation.gestures.*
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Star
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.soyvictorherrera.nosedive.presentation.theme.Dark_Pink
-import com.soyvictorherrera.nosedive.presentation.theme.Grey
 import com.soyvictorherrera.nosedive.presentation.theme.NosediveTheme
 import com.soyvictorherrera.nosedive.presentation.theme.Wild_Watermelon
-import timber.log.Timber
 
 @Composable
-@ExperimentalMaterialApi
 fun RatingBar(
     rating: Int,
     modifier: Modifier = Modifier,
-    maxValue: Int = 5
+    maxValue: Int = 5,
+    onRateChange: (newRate: Int) -> Unit
+) = Row(
+    modifier = modifier
 ) {
-    // TODO: 03/01/2022 cambiar esto por draggable para tener acceso a los valores
-    //  https://developer.android.com/jetpack/compose/gestures#dragging
-    val swipeableState = rememberSwipeableState(0)
-    var size by remember { mutableStateOf(Size.Zero) }
-    val anchors = mapOf(0f to 0, size.width to 1)
-    Row(
-        modifier
-            .onSizeChanged {
-                size = Size(width = it.width.toFloat(), height = it.height.toFloat())
-            }
-            .swipeable(
-                state = swipeableState,
-                anchors = anchors,
-                orientation = Orientation.Horizontal
-            )
-    ) {
-        (1..maxValue).forEach { step ->
-            RatingStar(
-                filled = step <= rating,
-                modifier = Modifier
-                    .size(64.dp)
-                    .padding(all = 8.dp)
-            )
-        }
+    (1..maxValue).forEach { step ->
+        RatingStar(
+            filled = step <= rating,
+            modifier = Modifier
+                .aspectRatio(
+                    ratio = 1f,
+                    matchHeightConstraintsFirst = true
+                )
+                .weight(1f)
+                .padding(all = 8.dp)
+                .clickable {
+                    onRateChange(step)
+                }
+        )
     }
 }
 
@@ -75,7 +59,9 @@ fun RatingStar(
 fun RatingBarPreview() {
     NosediveTheme {
         RatingBar(
-            rating = 4
+            rating = 4,
+            maxValue = 10,
+            onRateChange = {}
         )
     }
 }
--- app/src/main/java/com/soyvictorherrera/nosedive/presentation/ui/rateUser/RateUserContentView.kt
+++ app/src/main/java/com/soyvictorherrera/nosedive/presentation/ui/rateUser/RateUserContentView.kt
@@ -1,16 +1,23 @@
 package com.soyvictorherrera.nosedive.presentation.ui.rateUser
 
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.ExpandLess
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.soyvictorherrera.nosedive.R
@@ -19,7 +26,8 @@ import com.soyvictorherrera.nosedive.presentation.component.profile.UserPhoto
 import com.soyvictorherrera.nosedive.presentation.component.rate.RatingBar
 import com.soyvictorherrera.nosedive.presentation.theme.Grey
 import com.soyvictorherrera.nosedive.presentation.theme.NosediveTheme
-import com.soyvictorherrera.nosedive.presentation.theme.Wild_Watermelon
+import timber.log.Timber
+import kotlin.math.roundToInt
 
 @Composable
 fun RateUserContentView(
@@ -30,7 +38,12 @@ fun RateUserContentView(
     },
     content = {
         RateUserContent(
-            modifier = Modifier.padding(start = 32.dp, end = 32.dp, bottom = 64.dp)
+            modifier = Modifier.padding(
+                start = 32.dp,
+                end = 32.dp,
+                top = 16.dp,
+                bottom = 64.dp
+            )
         )
     }
 )
@@ -56,30 +69,89 @@ fun RateUserContent(
 
     Spacer(modifier = Modifier.weight(1f))
 
-    Icon(
-        imageVector = Icons.Sharp.ExpandLess,
-        tint = Grey,
-        contentDescription = "",
-        modifier = Modifier.size(24.dp)
+    var rating by remember { mutableStateOf(0) }
+    RateUserBar(
+        rating = rating,
+        onRateChange = { newRate ->
+            rating = newRate.also {
+                Timber.d("new rate is [$it]")
+            }
+        },
+        onRateSubmit = {
+            Timber.d("Submit new rate here")
+        }
     )
+}
 
-    Spacer(modifier = Modifier.height(16.dp))
+@Composable
+fun RateUserBar(
+    rating: Int,
+    modifier: Modifier = Modifier,
+    onRateChange: (newRate: Int) -> Unit,
+    onRateSubmit: () -> Unit
+) {
+    var offsetY by remember { mutableStateOf(0f) }
+    val threshold = with(LocalDensity.current) { 64.dp.toPx() * -1 }
+    val dragProgress = (offsetY / threshold)
+    val hintAlpha = (1f - dragProgress)
+    Column(
+        modifier = modifier
+            .offset { IntOffset(0, offsetY.roundToInt()) }
+            .draggable(
+                orientation = Orientation.Vertical,
+                state = rememberDraggableState { delta ->
+                    val offset = offsetY + delta
+                    // Allow drag upwards only
+                    if (offset < 0) {
+                        offsetY = (if (offset < threshold) threshold else offset)
+                    }
+                },
+                onDragStopped = {
+                    // Submit rating if view is full dragged
+                    if (offsetY <= threshold) onRateSubmit()
+                    offsetY = 0f
+                }
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Sharp.ExpandLess,
+            tint = Grey,
+            contentDescription = "",
+            modifier = Modifier
+                .size(24.dp)
+                .alpha(hintAlpha)
+        )
 
-    Text(
-        text = "Selecciona y desliza hacia arriba",
-        fontSize = 12.sp,
-        color = Grey
-    )
+        Spacer(modifier = Modifier.height(16.dp))
 
-    Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Selecciona y desliza hacia arriba",
+            fontSize = 12.sp,
+            color = Grey,
+            modifier = Modifier.alpha(hintAlpha)
+        )
 
-    RatingBar(
-        rating = 2,
-        modifier = Modifier
-            .height(64.dp)
-            .fillMaxWidth()
-    )
+        Spacer(modifier = Modifier.height(16.dp))
 
+        RatingBar(
+            rating = rating,
+            modifier = Modifier
+                .height(64.dp)
+                .fillMaxWidth(),
+            onRateChange = { onRateChange(it) }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        CircularProgressIndicator(
+            progress = dragProgress,
+            color = Grey,
+            modifier = Modifier
+                .size(32.dp)
+                .alpha(dragProgress)
+        )
+    }
 }
 
 @Preview
