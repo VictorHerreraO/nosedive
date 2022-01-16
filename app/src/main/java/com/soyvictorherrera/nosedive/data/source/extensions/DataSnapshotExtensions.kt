b65cf0786af50c2eed4dc80b1fb1d22f7975664d
VictorHerreraO <vherrera@overcome.mx>
2022-01-16 12:25:48 -0600

    Extensions: agregado DataSnapshot.getValues

--- app/src/main/java/com/soyvictorherrera/nosedive/data/source/extensions/DataSnapshotExtensions.kt
+++ app/src/main/java/com/soyvictorherrera/nosedive/data/source/extensions/DataSnapshotExtensions.kt
@@ -1,9 +1,30 @@
 package com.soyvictorherrera.nosedive.data.source.extensions
 
 import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ktx.getValue
+import com.soyvictorherrera.nosedive.data.source.IdentifiableEntity
 
 fun DataSnapshot.forEach(action: (snapshot: DataSnapshot) -> Unit) {
     for (child in children) {
         action(child)
+    }
+}
+
+/**
+ * Convierte el contenido del DataSnapshot en un listado de POJOs
+ *
+ * Usa [IdentifiableEntity] para poder asignar el ID al pojo
+ *
+ * Únicamente agrega snpashots cuyo valor != null
+ *
+ * @see [DataSnapshot.getValue]
+ */
+inline fun <reified T : IdentifiableEntity> DataSnapshot.getValues(): List<T> {
+    return mutableListOf<T>().apply {
+        this@getValues.forEach { child ->
+            child.getValue<T>()
+                ?.apply value@{ this@value.id = child.key }
+                ?.also { add(it) }
+        }
     }
 }
